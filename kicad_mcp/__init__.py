d866184d864871d5de60e9e0379a67000add0571
Lama <alrajihlama@gmail.com>
2025-07-08 19:49:17 -0400

    Add repo to pip package

--- kicad_mcp/__init__.py
+++ kicad_mcp/__init__.py
@@ -1,4 +1,12 @@
 """
 KiCad MCP Server - A Model Context Protocol server for KiCad.
 """
+from .server import *
+from .config import *
+from .context import *
+
 __version__ = "0.1.0"
+__author__ = "Lama Al Rajih"
+__description__ = "Model Context Protocol server for KiCad on Mac, Windows, and Linux"
+
+__all__ = [] # TODO: need to finish this
