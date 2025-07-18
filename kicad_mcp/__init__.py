2b425d458dc83e69b7f2f8e5b5575413fab5c90b
Lama <alrajihlama@gmail.com>
2025-07-18 01:57:04 -0400

    update init and remove email placeholder from pyproject.toml

--- kicad_mcp/__init__.py
+++ kicad_mcp/__init__.py
@@ -9,4 +9,19 @@ __version__ = "0.1.0"
 __author__ = "Lama Al Rajih"
 __description__ = "Model Context Protocol server for KiCad on Mac, Windows, and Linux"
 
-__all__ = [] # TODO: need to finish this
+__all__ = [
+    # Package metadata
+    "__version__",
+    "__author__",
+    "__description__",
+
+    # Server creation / shutdown helpers
+    "create_server",
+    "add_cleanup_handler",
+    "run_cleanup_handlers",
+    "shutdown_server",
+
+    # Lifespan / context helpers
+    "kicad_lifespan",
+    "KiCadAppContext",
+]
